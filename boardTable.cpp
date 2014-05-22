084a02cbf5e58d58aa2a2d62ac6874023669f288
NoNameA 774 <nonamea774@gmail.com>
2014-05-23 00:38:10 +0900

    実験的に消してたコメントアウトを削除

--- boardTable.cpp
+++ boardTable.cpp
@@ -9,7 +9,6 @@ namespace {
             bool joined = false;
             bool hit = false;
             for(int i(0); i < 4;++i){
-                // joined = false;
                 if(Board::get(grid, k, i) == 0) continue;
                 hit = false;
                 for(int j(i-1); j >= 0;--j){
@@ -31,10 +30,8 @@ namespace {
                 if(i != 0 && ! hit){
                     grid = Board::set(grid, k, 0, Board::get(grid, k, i));
                     grid = Board::set(grid, k, i, 0);
-                    // joined = false;
                 }
             }
-            // printf("%u\n", grid);
             table[orig] = grid;
         }
         return table;
@@ -48,19 +45,4 @@ Board::Grid Board::moveLeft(Board::Grid grid){
         ((uint64_t)table[(grid & UINT64_C(0x00000000FFFF0000)) >> 16] << 16) |
         ((uint64_t)table[(grid & UINT64_C(0x000000000000FFFF)) >> 00] << 00) ;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
