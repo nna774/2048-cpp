7cb015c2525ea61f22ffc648b1e8f735c7ae6d28
NoNameA 774 <nonamea774@gmail.com>
2014-05-29 04:47:43 +0900

    不適切なマスク処理だったことをお詫びします

--- board.cpp
+++ board.cpp
@@ -186,7 +186,8 @@ std::array<uint16_t, 1 << 16> Board::makeTable(){
             for(int j(i-1); j >= 0;--j){
                 if(Board::get(grid, k, j) == 0) continue;
                 if(Board::get(grid, k, j) == Board::get(grid, k, i) && !joined){
-                    grid = Board::set(grid, k, j, (Board::get(grid, k, j) + 1) & 0b1111);
+                    auto const gotten = Board::get(grid, k, j);
+                    grid = Board::set(grid, k, j, gotten == 0b1111 ? gotten : gotten + 1);
                     grid = Board::set(grid, k, i, 0);
                     joined = true;
                 }else{
